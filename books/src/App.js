4d632a7221057d07b6a0e99129d94ebd7549c99b
Javier Ruedas <jruedas@twc.com>
2024-03-13 08:27:28 -0500

    implement REST routes

--- books/src/App.js
+++ books/src/App.js
@@ -1,9 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App(){
     const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    }
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
     const editBookById = (id, newTitle) => {
         const updatedBooks = books.map(book => {
@@ -23,11 +33,15 @@ function App(){
         setBooks(updatedBooks);
     }
 
-    const createBook = (title) => {
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+
+        console.log(response);
         const updatedBooks = [
             ...books,
-            { id: Math.round(Math.random() * 99999),
-                title }
+            response.data
         ];
         setBooks(updatedBooks);
     }
