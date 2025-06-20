a9ac301e69eaaa8aac0c171f7a074777ad6a4eda
Javier Ruedas <jruedas@twc.com>
2025-06-20 17:11:00 -0500

    build Button component and ButtonPage

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,30 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { v4 as uuidv4 } from 'uuid';
+import Accordion from "./components/Accordion";
 
-function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+function App(){
+    const items = [
+        {
+            id: uuidv4(),
+            label: 'Using React',
+            content: 'React makes it easier to build dynamic user interfaces by managing state and rendering components efficiently. Its component-based structure promotes reusability and cleaner code organization.'
+        },
+        {
+            id: uuidv4(),
+            label: 'Using vanilla JS',
+            content: "Vanilla JavaScript gives you full control over the DOM without relying on external libraries. It's lightweight and ideal for small projects or when you want to avoid the overhead of frameworks."
+        },
+        {
+            id: uuidv4(),
+            label: 'Custom CSS or frameworks',
+            content: 'Custom CSS offers complete design flexibility and helps keep your styles minimal and tailored to your project. CSS frameworks like Bootstrap or Tailwind speed up development with prebuilt utilities but can add extra bloat if not managed carefully.'
+        }
+    ];
+
+    return (
+        <div>
+            <Accordion items={items} />
+        </div>
+    );
 }
 
 export default App;
---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,18 @@
+import Button from "../components/Button";
+import {GoBell, GoCloudDownload, GoDatabase} from "react-icons/all";
+
+function ButtonPage(){
+    const handleClick = () => console.log('click!');
+
+    return (
+        <div>
+            <div><Button onClick={handleClick} success rounded outline className='mb-5'><GoBell />Click Me!</Button></div>
+            <div><Button danger outline><GoCloudDownload />Buy Now</Button></div>
+            <div><Button warning><GoDatabase />See Deal</Button></div>
+            <div><Button secondary outline>Hide Adds</Button></div>
+            <div><Button primary rounded>See Similar</Button></div>
+        </div>
+    );
+}
+
+export default ButtonPage;
