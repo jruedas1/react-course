c1482d37dd39bcc0f8ac021d3d5575ff9b73e8ed
Javier Ruedas <jruedas@twc.com>
2025-07-09 09:55:17 -0500

    start building dropdown component

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,30 +1,13 @@
-import { v4 as uuidv4 } from 'uuid';
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App(){
-    const items = [
-        {
-            id: uuidv4(),
-            label: 'Using React',
-            content: 'React makes it easier to build dynamic user interfaces by managing state and rendering components efficiently. Its component-based structure promotes reusability and cleaner code organization.'
-        },
-        {
-            id: uuidv4(),
-            label: 'Using vanilla JS',
-            content: "Vanilla JavaScript gives you full control over the DOM without relying on external libraries. It's lightweight and ideal for small projects or when you want to avoid the overhead of frameworks."
-        },
-        {
-            id: uuidv4(),
-            label: 'Custom CSS or frameworks',
-            content: 'Custom CSS offers complete design flexibility and helps keep your styles minimal and tailored to your project. CSS frameworks like Bootstrap or Tailwind speed up development with prebuilt utilities but can add extra bloat if not managed carefully.'
-        }
-    ];
+    const options = [
+        { label: 'Red', value: 'red' },
+        { label: 'Green', value: 'green' },
+        { label: 'Blue', value: 'blue' }
+    ]
 
-    return (
-        <div>
-            <Accordion items={items} />
-        </div>
-    );
+   return <Dropdown options={options} />
 }
 
 export default App;
---
+++ comps/src/pages/AccordionPage.js
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from 'uuid';
+import Accordion from "../components/Accordion";
+
+function AccordionPage(){
+    const items = [
+        {
+            id: uuidv4(),
+            label: 'Using React',
+            content: 'React makes it easier to build dynamic user interfaces by managing state and rendering components efficiently. Its component-based structure promotes reusability and cleaner code organization.'
+        },
+        {
+            id: uuidv4(),
+            label: 'Using vanilla JS',
+            content: "Vanilla JavaScript gives you full control over the DOM without relying on external libraries. It's lightweight and ideal for small projects or when you want to avoid the overhead of frameworks."
+        },
+        {
+            id: uuidv4(),
+            label: 'Custom CSS or frameworks',
+            content: 'Custom CSS offers complete design flexibility and helps keep your styles minimal and tailored to your project. CSS frameworks like Bootstrap or Tailwind speed up development with prebuilt utilities but can add extra bloat if not managed carefully.'
+        }
+    ];
+
+    return (
+        <div>
+            <Accordion items={items} />
+        </div>
+    );
+}
+
+export default AccordionPage;
