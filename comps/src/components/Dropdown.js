e870104f8125a9b965984a43b8abbb5e68190d88
Javier Ruedas <jruedas@twc.com>
2025-07-11 16:07:59 -0500

    create reusable Panel component

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,20 +1,38 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from './Panel';
 
-function Dropdown({ options }){
+function Dropdown({ options, value, onChange }){
     const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = () => {
         setIsOpen(prev => !prev);
     }
 
-    const renderedOptions = options.map(option=>{
-        return <div key={option.value}>{option.label}</div>
+    const handleOptionClick = (option) => {
+        setIsOpen(false);
+        onChange(option);
+    }
+
+    const renderedOptions = options.map(option => {
+        return <div
+            className="hover:bg-sky-100 rounded cursor-pointer p-1"
+            onClick={() => handleOptionClick (option)}
+            key={option.value}
+        >
+            {option.label}
+        </div>
     });
 
     return (
-        <div>
-            <div onClick={handleClick}>Select...</div>
-            {isOpen && <div>{renderedOptions}</div>}
+        <div className="w-48 relative">
+            <Panel onClick={handleClick}
+                 className="flex justify-between items-center cursor-pointer"
+            >
+                {value?.label || 'Select...'}
+                <GoChevronDown className="text-lg" />
+            </Panel>
+            {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
         </div>
     );
 }
