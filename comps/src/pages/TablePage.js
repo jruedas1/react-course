bed543aebdd729ea3b4c72b8f6469a211e5aedd5
Javier Ruedas <jruedas@twc.com>
2025-07-18 12:09:45 -0500

    complete sortable table component

--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage(){
 
@@ -12,7 +12,8 @@ function TablePage(){
     const config = [
         {
             label: 'Name',
-            render: fruit => fruit.name
+            render: fruit => fruit.name,
+            sortValue: fruit => fruit.name
         },
         {
             label: 'Color',
@@ -20,13 +21,14 @@ function TablePage(){
         },
         {
             label: 'Score',
-            render: fruit => fruit.score
+            render: fruit => fruit.score,
+            sortValue: fruit => fruit.score
         }
     ];
 
     const keyFn = fruit => fruit.name;
 
-    return <div><Table data={data} config={config} keyFn={keyFn} /></div>
+    return <div><SortableTable data={data} config={config} keyFn={keyFn} /></div>
 }
 
 export default TablePage;
